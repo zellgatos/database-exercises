ccb17ec77f9743c0d03b0e96c3d2e01f229d12c5
zellgatos <zellbrawl@yahoo.com>
2017-10-13 09:34:15 -0500

    attempting to fix branches on repo...

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,5 @@
 USE codeup_test_db
+
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson','Thriller',1982,47.3,'Pop, Rock, R&B' ),
  ('Eagles', 'Their Greatest Hits (1971-1975)',1976,32.2, 'Rock, Soft Rock, Folk Rock'),
